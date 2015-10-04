fbac96076b95b0302434e541808168e034cc346c
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-04 05:35:25 +0000

    added calculation for bottle exchange

--- bottle_algorithm.rb
+++ bottle_algorithm.rb
@@ -1,3 +1,15 @@
 class BottleAlgorithm
 
-  def initialize(bottles, caps, freebies)
+  def initialize(bottleswap, capswap, eachpop)
+    @bottleswap = 4
+    @caps = 8
+    @eachpop = 2
+  end
+
+  def information
+    bottles_bought = @amount % @eachpop
+    puts '-' * 50
+    puts "For the #{customer.amount}You will receive #{bottles_bought}"
+  end
+
+end
--- user_interface.rb
+++ user_interface.rb
@@ -1,10 +1,10 @@
 require_relative 'customer'
-require_relative 'bottle_algorithm
-'
+require_relative 'bottle_algorithm'
+
 #asks customer for the amount they will invest for the day
 
 puts "How much will you spend today?"
-customer = Customer.new(get.chomp.to_i)
+customer = Customer.new(gets.chomp.to_i)
 
 #start bottle algorightm for the new customer
 new_customer = BottleAlgorithm.new(customer)
